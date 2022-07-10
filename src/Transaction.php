0fcf98d7cef3d7870cee8a67404c8bc440274a17
Matthew Baggett <matthew@baggett.me>
2022-07-10 06:34:44 +0200

    php-cs-fixer.

--- src/Transaction.php
+++ src/Transaction.php
@@ -1,35 +1,42 @@
 <?php
+
 namespace Benzine\ORM;
 
 use Benzine\App;
 use Benzine\ORM\Connection\Database;
 use Benzine\ORM\Connection\Databases;
 
-class Transaction{
-
-    public static function Begin(){
+class Transaction
+{
+    public static function Begin(): void
+    {
         /** @var Databases $databases */
         $databases = App::Instance()->get(Databases::class);
+
         /** @var Database $database */
-        foreach($databases as $database){
+        foreach ($databases as $database) {
             $database->getAdapter()->getDriver()->getConnection()->beginTransaction();
         }
     }
 
-    public static function Commit(){
+    public static function Commit(): void
+    {
         /** @var Databases $databases */
         $databases = App::Instance()->get(Databases::class);
+
         /** @var Database $database */
-        foreach($databases as $database){
+        foreach ($databases as $database) {
             $database->getAdapter()->getDriver()->getConnection()->commit();
         }
     }
 
-    public static function Rollback(){
+    public static function Rollback(): void
+    {
         /** @var Databases $databases */
         $databases = App::Instance()->get(Databases::class);
+
         /** @var Database $database */
-        foreach($databases as $database){
+        foreach ($databases as $database) {
             $database->getAdapter()->getDriver()->getConnection()->rollback();
         }
     }
